1b613df9a5015cbb4a3e5e882bc0d6cbad327190
diegosilveiracn <diegoscnascimento@gmail.com>
2015-12-07 22:46:15 -0300

    Incluindo 'dependent=true' no mapeamento.

--- src/app/Model/Clinica.php
+++ src/app/Model/Clinica.php
@@ -2,7 +2,11 @@
 
 class Clinica extends AppModel{
 
-	public $name = "Clinica";
+	 public $name = "Clinica";
+
+	 public $hasMany = array(
+		  'Consulta' => array('dependent' => true)
+	 );
 
 	 public $validate = array(
         'nome' => array(
--- src/app/Model/Oftalmologista.php
+++ src/app/Model/Oftalmologista.php
@@ -4,12 +4,20 @@ class Oftalmologista extends AppModel{
 
     public $name = 'Oftalmologista';
 
-	public $validate = array(
+    public $hasMany = array(
+ 		  'Consulta' => array('dependent' => true)
+ 	  );
+
+    public $hasOne = array(
+  		'User' => array('dependent' => true)
+  	);
+
+	  public $validate = array(
 			'nome' => array(
 									'rule' => 'notEmpty',
 	    						'message' => 'O campo "nome" é obrigatorio!'
 								)
-	);
+	  );
 }
 
 ?>
